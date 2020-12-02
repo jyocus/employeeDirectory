d7cfd72d1ce3e8c01709867da094d026fa4f2b7b
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-02 16:33:48 -0600

    components completed minus styling so I can attempt to make data appear in the right place

--- client/src/components/employee/card.js
+++ client/src/components/employee/card.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./style.css"
 
+
+//https://getbootstrap.com/docs/4.0/layout/grid/
 function card() {
     return (
-        <div>
-            
+        <div className="container">
+            <div className="row">
+                {/* Some kind of click event needs to happen here */}
+                <div className="col-sm">
+                    <h2>Column 1</h2>
+                </div>
+
+                <div className="col-sm">
+                    <h2>Column 2</h2>
+                </div>
+
+                <div className="col-sm">
+                    <h2>Column 3</h2>
+                </div>
+
+            </div>
+
         </div>
     )
 }
--- client/src/components/table/label.js
+++ client/src/components/table/label.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./style.css"
 
+//https://getbootstrap.com/docs/4.0/layout/grid/
 function label() {
     return (
-        <div>
+        <div className="container">
+            <div className="row">
             
+                <div className="col-sm">
+                    <h2>Label 1</h2>
+                </div>
+
+                <div className="col-sm">
+                    <h2>Label 2</h2>
+                </div>
+
+                <div className="col-sm">
+                    <h2>Label 3</h2>
+                </div>
+
+            </div>
+
         </div>
     )
 }
