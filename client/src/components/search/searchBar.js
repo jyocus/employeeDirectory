ed21d1f75bb4b437c57db5109c3a59d55adb4b3d
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-02 16:08:56 -0600

    simple search bar added and rendering

--- client/src/components/search/searchBar.js
+++ client/src/components/search/searchBar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function searchBar() {
+function searchBar({handleSearchChange}) {
     return (
         <div>
-            
+            <form className="form-inline">
+                <input
+                    className="form-control mr-lg-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={e => handleSearchChange(e)}
+                />
+                <button className="btn my-2 my-sm-0" type="submit">
+                    Search
+                </button>
+            </form>
+
         </div>
     )
 }
