5f69aa4315b1bf515d6e1d68f08d858b10dd1666
Justin Yocus <justin.o.yocus@gmail.com>
2020-12-02 16:22:00 -0600

    nav/header created, still some styling left to do

--- client/src/components/header/nav.js
+++ client/src/components/header/nav.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 function nav() {
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar">
+            <span className="nav-text">Employee Directory</span>
+            <p className="nav-para">Sort and Filter Through The Employees Below</p>
+        </nav>
     )
 }
 
-export default nav
+export default nav;
